27d73cd399e823a38aa57cc23dfbf3f7c97787b3
Jim Jiang <jim@lotlab.org>
2022-09-12 17:18:12 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <p>{{ text }}</p>
+    <button @click="onClick()">button</button>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Setup, Context } from 'vue-class-setup';
+
+// Setup and Context must work together
+@Setup
+class Home extends Context {
+  private _value = 0;
+  public get text() {
+    return String(this._value);
+  }
+  public set text(text: string) {
+    this._value = Number(text);
+  }
+  public onClick() {
+    this._value++;
+  }
+}
+export default defineComponent({
+  // Inject setup
+  ...Home.inject(),
+});
+</script>
