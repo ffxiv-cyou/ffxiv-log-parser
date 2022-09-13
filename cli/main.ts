47331a32ffb817cc9f9428fa14d42c09153e355f
Jim Jiang <jim@lotlab.org>
2022-09-13 21:49:55 +0800

    添加调试用cli

---
+++ cli/main.ts
@@ -0,0 +1,77 @@
+import { readFile } from "fs/promises";
+import { argv, exit } from "process";
+
+import { Message, BinLogParser, buf2hex } from "../src/model/binlog_parser";
+
+if (argv.length < 3) {
+    console.log("Usage: converter <path1>");
+    exit(-1);
+}
+
+readFiles(argv.slice(2)).then(() => {});
+
+function readFiles(files: string[]): Promise<any> {
+    return new Promise<any>(async (resolve, reject) => {
+        const parser = new BinLogParser();
+        let msgs : Message[] = [];
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const buffer = await readFile(file);
+
+            const msg = parser.parse(toArrayBuffer(buffer));
+            msgs.push(...msg);
+            console.log(file, msg.length);
+        }
+
+        const filterSet = new Set<number>();
+        const cmdSet = new Set<number>();
+
+        for (let i = 0; i < msgs.length; i++) {
+            const msg = msgs[i];
+            filterSet.add(msg.filter);
+
+            let printK = false;
+
+            for (let j = 0; j < msg.sender.items.length; j++) {
+                const it = msg.sender.items[j];
+                cmdSet.add(it.cmd);
+
+                if (it.cmd == 0x2E) {
+                    console.log(buf2hex(it.param));
+                    printK = true;
+                }
+            }
+            for (let j = 0; j < msg.text.items.length; j++) {
+                const it = msg.text.items[j];
+                cmdSet.add(it.cmd);
+
+                if (it.cmd == 0x2E) {
+                    console.log(buf2hex(it.param));
+                    printK = true;
+                }
+            }
+
+            if (printK) {
+                console.log(msg.sender.ToString(), msg.text.ToString());
+            }
+        }
+        console.log(toHexArray(Uint8Array.from(filterSet).sort()));
+        console.log(toHexArray(Uint8Array.from(cmdSet).sort()));
+
+        resolve("");
+    });
+}
+
+function toArrayBuffer(buf) {
+    const ab = new ArrayBuffer(buf.length);
+    const view = new Uint8Array(ab);
+    for (let i = 0; i < buf.length; ++i) {
+        view[i] = buf[i];
+    }
+    return ab;
+}
+
+function toHexArray(array) {
+    return [...array].map((n) => n.toString(16));
+}
