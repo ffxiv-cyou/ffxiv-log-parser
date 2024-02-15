48e6085f09c0b6ed728a38e83aeb628723b76b15
Jim Kirisame <jim@lotlab.org>
2024-02-15 20:35:20 +0800

    fix: 修正LogFilter不正确的问题

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@ export default class App extends Vue {
   filter = new Map<number, boolean>();
 
   get filterMessages(): Message[] {
-    return this.messages.filter((x) => this.filter.get(x.filter) === true);
+    return this.messages.filter((x) => this.filter.get(x.filter & 0x7F) === true);
   }
 
   longTime = false;
